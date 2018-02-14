d7be2c4b76e922ee5d83343725a9c58b62d79d54
Erik Ilyin <erik.ilyin@aol.com>
2018-02-14 11:46:09 +0300

    router fix

--- server/routes/index.js
+++ server/routes/index.js
@@ -12,14 +12,13 @@ let register = app => {
     router.use('/api', [auth, act, answer, user]);
     router.use(express.static(path.join(__dirname, '../../client/build')));
     app.use(router);
-    app.use((req,res,next)=> {
-        let error =new Error('Not Found');
-        error.status = 404;
-        next(error);
-    });
     app.use((error, req, res, next) => {
         res.status(error.status || 500).json({success: false, data: null, error, message: error.message})
     });
+
+    app.get('*', (req,res) => {
+        res.sendFIle(path.join(__dirname, '../../client/build/index.html'))
+    })
 }
 
 export default register;
