604c8782e2ac671530e3249943deced7104b6831
Erik Ilyin <erik.ilyin@aol.com>
2018-04-12 20:14:22 +0800

    underscore seed

--- server/seeders/20180118032529-super-admin-user.js
+++ server/seeders/20180118032529-super-admin-user.js
@@ -3,29 +3,32 @@ var bcrypt = require('bcrypt');
 module.exports = {
   up: (queryInterface, Sequelize) => {
     let hash = bcrypt.hashSync('admin2mindlogger', 10)
-    return queryInterface.bulkInsert('Users', [{
+    return queryInterface.bulkInsert('users', [{
       first_name: 'Super Admin',
       last_name: '',
       role: 'super_admin',
       email: 'admin@mindlogger.org',
       status: 'active',
       password: hash,
-      createdAt : new Date(),
-      updatedAt : new Date()
-    }], {})
-    if(process.env.NODE_ENV != 'production' ) {
-      queryInterface.bulkInsert('Users', [{
-        first_name: 'John',
-        last_name: 'Doe',
-        role: 'admin',
-        email: 'tester@test.com',
-        role: 'admin',
-        status: 'active',
-        password: hash,
-        createdAt : new Date(),
-        updatedAt : new Date(),
-      }], {})
-    }
+      created_at : new Date(),
+      updated_at : new Date()
+    }], {}).then( res => {
+      if(process.env.NODE_ENV != 'production' ) {
+        return queryInterface.bulkInsert('Users', [{
+          first_name: 'John',
+          last_name: 'Doe',
+          role: 'admin',
+          email: 'tester@test.com',
+          role: 'admin',
+          status: 'active',
+          password: hash,
+          created_at : new Date(),
+          updated_at : new Date(),
+        }], {})
+      } else {
+        return true;
+      }
+    });
     
     /*
       Add altering commands here.
