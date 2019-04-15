c37cb9b0326d4d4f64685599cf30c393592acf11
Jon Clucas <jon@vasegurt.com>
2019-04-15 17:50:00 -0400

    :construction: :tractor: Refactor `GET responses` Endpoint (WIP)
    Ref https://github.com/ChildMindInstitute/mindlogger-web/issues/10

--- girder/api/v1/response.py
+++ girder/api/v1/response.py
@@ -24,10 +24,14 @@ from girder.utility import ziputil
 from girder.constants import AccessType, TokenScope
 from girder.exceptions import AccessException, RestException, ValidationException
 from girder.api import access
+from girder.api.v1.applet import getCanonicalUser
+from girder.api.v1.context import listFromString
+from girder.models.activity import Activity as ActivityModel
 from girder.models.folder import Folder
 from girder.models.item import Item as ItemModel
 from girder.models.user import User as UserModel
 from girder.models.upload import Upload as UploadModel
+import itertools
 import tzlocal
 
 
@@ -40,32 +44,214 @@ class ResponseItem(Resource):
         self.route('GET', (), self.getResponses)
         self.route('POST', (), self.createResponseItem)
 
-
+    """
+    TODO 🚧:
+        .param(
+            'screen',
+            'The ID of the screen for which to get responses or an Array '
+            'thereof.',
+            required=False
+        )
+    """
     @access.public(scope=TokenScope.DATA_READ)
     @autoDescribeRoute(
-        Description('Get all responses for a given user and applet.')
-        .param('userId', 'The ID of the user for whom to get responses.', required=True)
-        .param('appletId', 'The ID of the applet for which to get responses.', required=True)
+        Description(
+            'Get all responses for a given user, applet, and/or activity. '
+            'Parameters act as cumulative filters, so mutually '
+            'exclusive combinations will return an empty Array.'
+        )
+        .param(
+            'respondent',
+            'The ID (canonical or applet-specific) of the respondent for whom '
+            'to get responses or an Array thereof.',
+            required=False
+        )
+        .param(
+            'subject',
+            'The ID (canonical or applet-specific) of the subject about whom '
+            'to get responses or an Array thereof.',
+            required=False
+        )
+        .param(
+            'applet',
+            'The ID of the applet for which to get responses or an Array '
+            'thereof.',
+            required=False
+        )
+        .param(
+            'activity',
+            'The ID of the activity for which to get responses or an Array '
+            'thereof.',
+            required=False
+        )
         .errorResponse('ID was invalid.')
         .errorResponse(
             'Read access was denied for this applet for this user.',
             403
         )
     )
-    def getResponses(self, userId, appletId):
+    def getResponses(
+        self,
+        respondent=[],
+        subject=[],
+        applet=[],
+        activity=[],
+        screen=[]
+    ):
         reviewer = self.getCurrentUser()
+        allResponses = []
+        try:
+            respondent = listFromString(respondent)
+            respondents = list(set([cu for cu in
+                [
+                    getCanonicalUser(u) for u in respondent
+                ] if cu is not None
+            ]))
+        except:
+            raise ValidationException(
+                'Invalid parameter.',
+                'respondent'
+            )
+        try:
+            subject = listFromString(subject)
+            subjects = list(set([cu for cu in
+                [
+                    getCanonicalUser(u) for u in subject
+                ] if cu is not None
+            ]))
+        except:
+            raise ValidationException(
+                'Invalid parameter.',
+                'subject'
+            )
+        try:
+            applets = listFromString(applet)
+            # TODO: validate applet ID and/or URL
+        except:
+            raise ValidationException(
+                'Invalid parameter.',
+                'applet'
+            )
+        try:
+            activities = listFromString(activity)
+            activities = [
+                *list(itertools.chain.from_iterable(
+                    ActivityModel().listVersionId(
+                        id=activity,
+                        user=reviewer
+                    ) for activity in activities
+                ))
+            ]
+        except:
+            raise ValidationException(
+                'Invalid parameter.',
+                'activity'
+            )
+        try:
+            screens = listFromString(screen)
+        except:
+            raise ValidationException(
+                'Invalid parameter.',
+                'screen'
+            )
+        del respondent, subject, applet, activity, screen
+        return(", ".join([str(respondents), str(subjects), str(applets), str(activities), str(screens)]))
+        ## 🚧 continue from here
+        for respondent in respondents:
+            allResponses.append(
+                *this._getUserResponses(
+                    respondent=respondent,
+                    subjects=subjects,
+                    applets=applets,
+                    activities=activities,
+                    screens=screens
+                )
+            )
+        return(allResponses)
+
+    def _getUserResponsesFolder(self, user):
         user = UserModel().load(
-            id=userId, user=reviewer, level=AccessType.NONE, exc=True
-        )
-        folder = Folder().load(
-            id=appletId, user=user, level=AccessType.NONE, exc=True
+            id=user, user=reviewer, level=AccessType.NONE, exc=True
         )
         UserResponsesFolder = Folder().createFolder(
             parent=user, parentType='user', name='Responses', creator=user,
             reuseExisting=True, public=False)
+        return(UserResponsesFolder)
+
+
+    def _getUserResponses(
+        self,
+        respondent,
+        subjects=[],
+        applets=[],
+        activities=[],
+        screens=[]
+    ):
+        UserResponsesFolder = this._getUserResponsesFolder(respondent)
         UserAppletResponsesFolders = Folder().childFolders(
             parent=UserResponsesFolder, parentType='folder',
             user=reviewer)
+        allResponses = []
+        for appletResponsesFolder in UserAppletResponsesFolders:
+            folder = Folder().load(
+                id=appletResponsesFolder["_id"], user=reviewer,
+                level=AccessType.READ, exc=True
+            )
+            if len(subjects):
+                subjectFolders = {}
+                for subject in subjects:
+                    subjectFolders[subject] = Folder().childFolders(
+                        parent=folder, parentType='folder', user=reviewer,
+                        filters={'name': str(subject)}
+                    )
+            else:
+                subjectFolders = {
+                    responseFolder[
+                        'name'
+                    ]: responseFolder for responseFolder in Folder(
+                    ).childFolders(
+                        parent=folder,
+                        parentType='folder',
+                        user=reviewer
+                    )
+                }
+        if not len(applets): # don't filter by applet
+            for subjectFolder in subjectFolders:
+                allResponses += list(Folder().childItems(
+                    folder=subjectFolders[subjectFolder], user=reviewer
+                ))
+        else:
+            for applet in applets: # filter by applet
+                try:
+                    for subjectFolder in subjectFolders:
+                        allResponses += list(Folder().childItems(
+                            folder=subjectFolders[subjectFolder],
+                            user=reviewer,
+                            filters={
+                                '$or': [
+                                    {'meta.applet.@id': str(applet)},
+                                    {'meta.applet.url': str(applet)}
+                                ]
+                            }
+                        ))
+                except:
+                    pass
+        for activity in activities:
+            allResponses = [
+                response for response in allResponses if ((
+                    response.get('activity')==str(activity)
+                ) or (
+                    response.get('activity').get('@id')==str(activity)
+                ) or (
+                    response.get('activity').get('url')==str(activity)
+                ))
+            ]
+        return(allResponses)
+
+
+        folder = Folder().load(
+            id=appletId, user=user, level=AccessType.NONE, exc=True
+        )
         allResponses = {}
         for appletResponsesFolder in UserAppletResponsesFolders:
             if (
@@ -89,7 +275,6 @@ class ResponseItem(Resource):
                     allResponses[appletId] += list(Folder().childItems(
                         folder=subjectFolder, user=reviewer
                     ))
-        return(allResponses)
 
 
     @access.user(scope=TokenScope.DATA_WRITE)
