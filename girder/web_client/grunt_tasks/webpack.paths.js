deed9974aec71569f1f99be80895f363508cb2c5
Jon Clucas <jon@vasegurt.com>
2019-05-07 11:21:09 -0400

    no dunder, no slash

--- girder/web_client/grunt_tasks/webpack.paths.js
+++ girder/web_client/grunt_tasks/webpack.paths.js
@@ -3,5 +3,5 @@ var path = require('path');
 module.exports = {
     node_modules: path.resolve('node_modules'),
     plugins: path.resolve(__dirname, 'plugins'),
-    static: path.resolve('/static')
+    static: path.resolve('static')
 };
