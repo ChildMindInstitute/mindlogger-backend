9ed11f948640838f3723fd77df84c33aeb0964d1
Jon Clucas <jon.clucas@childmind.org>
2019-05-08 17:02:28 -0400

    rererebase

--- girder/constants.py
+++ girder/constants.py
@@ -28,7 +28,7 @@ VERSION = {
 #: The local directory containing the static content.
 STATIC_PREFIX = os.path.join(sys.prefix, 'share', 'girder')
 STATIC_ROOT_DIR = os.path.join(STATIC_PREFIX, 'static')
-
+print(STATIC_ROOT_DIR)
 PREFERRED_NAMES = ["skos:prefLabel", "skos:altLabel", "name", "@id", "url"]
 SPECIAL_SUBJECTS = {"ALL", "NONE"}
 USER_ROLES = {'user', 'editor', 'manager', 'reviewer'}
