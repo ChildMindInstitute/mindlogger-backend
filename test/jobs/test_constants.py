daeae20fc2e1e499f3da243206bb615e43cff78b
Jon Clucas <jon.clucas@childmind.org>
2019-09-25 12:16:49 -0400

    :yellow_heart: :fire: Delete Girder classic tests

--- test/jobs/test_constants.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from girder_jobs import constants
-
-
-@pytest.mark.plugin('jobs')
-def testConstantsIsDefined(server):
-    assert constants.JobStatus.isValid(constants.JobStatus.SUCCESS) is True
