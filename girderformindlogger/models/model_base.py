64cabb72042ce89c6c91b93fef9ac860775cab5c
Jon Clucas <jon.clucas@childmind.org>
2019-11-08 16:45:26 -0500

    :books: :speaker: Tell 404 message instead of 'name required' for 404s

--- girderformindlogger/models/model_base.py
+++ girderformindlogger/models/model_base.py
@@ -11,11 +11,12 @@ from bson.errors import InvalidId
 from pymongo.errors import WriteError
 from dictdiffer import diff
 from girderformindlogger import events, logprint, logger, auditLogger
-from girderformindlogger.constants import AccessType, CoreEventHandler, SortDir, \
-    ACCESS_FLAGS, PREFERRED_NAMES, TEXT_SCORE_SORT_MAX, USER_ROLES
+from girderformindlogger.constants import AccessType, CoreEventHandler,        \
+    SortDir, ACCESS_FLAGS, PREFERRED_NAMES, TEXT_SCORE_SORT_MAX, USER_ROLES
 from girderformindlogger.external.mongodb_proxy import MongoProxy
 from girderformindlogger.models import getDbConnection
-from girderformindlogger.exceptions import AccessException, ValidationException
+from girderformindlogger.exceptions import AccessException,                    \
+    ResourcePathNotFound, ValidationException
 
 USER_ROLE_KEYS = USER_ROLES.keys()
 
@@ -257,6 +258,10 @@ class Model(object):
             )
         passedUrl = url
         url = reprolibCanonize(url)
+        if url is None:
+            raise ResourcePathNotFound("Document not found: {}".format(str(
+                passedUrl
+            )))
         model = contextualize(loadJSON(url, modelType))
         atType = model.get('@type', '').split('/')[-1].split(':')[-1]
         modelType = firstLower(atType) if len(atType) else modelType
@@ -265,7 +270,7 @@ class Model(object):
         )=='activityset' else modelType
         changedModel = atType != modelType and len(atType)
         prefName = self.preferredName(model)
-        cachedDoc = self.getCached(url, modelType)
+        cachedDoc = self.getCached(passedUrl, modelType)
         if cachedDoc and not changedModel:
             if not refreshCache:
                 return(cachedDoc)
