d918dcfdb9532360c0d35948b390be0b371c3c6b
Benjamin Thonet <devtech728@gmail.com>
2020-07-20 20:15:17 +0300

    usedId changed to _id

--- girderformindlogger/external/notification.py
+++ girderformindlogger/external/notification.py
@@ -38,7 +38,7 @@ def send_push_notification(applet_id, event_id, activity_id=None, send_time=None
 
         if event['individualized']:
             query['individual_events'] = {'$gte': 1}
-            query['userId'] = {
+            query['_id'] = {
                 '$in': [ObjectId(user) for user in event['data']['users']]
             }
 
