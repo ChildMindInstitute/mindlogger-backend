073af5be4a819173565c07521aeb63c3b34a974a
Benjamin Thonet <devtech728@gmail.com>
2020-07-22 17:18:26 +0300

    new functional was added for notification performance using firebase_admin

--- girderformindlogger/external/notification.py
+++ girderformindlogger/external/notification.py
@@ -1,11 +1,15 @@
+import os
 import datetime
 
-from bson import ObjectId
-from pyfcm import FCMNotification
+import firebase_admin
+from firebase_admin import messaging, credentials
 
-push_service = FCMNotification(
-        api_key='AAAAJOyOEz4:APA91bFudM5Cc1Qynqy7QGxDBa-2zrttoRw6ZdvE9PQbfIuAB9SFvPje7DcFMmPuX1IizR1NAa7eHC3qXmE6nmOpgQxXbZ0sNO_n1NITc1sE5NH3d8W9ld-cfN7sXNr6IAOuodtEwQy-',
-        proxy_dict={})
+AMOUNT_MESSAGES_PER_REQUEST = 500
+REQUESTS_LIMITATION_PER_EVENT = 230
+
+ABSOLUTE_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'serviceAccountKey.json')
+cred = credentials.Certificate(ABSOLUTE_PATH)
+app = firebase_admin.initialize_app(cred)
 
 
 def send_push_notification(applet_id, event_id, activity_id=None, send_time=None):
@@ -67,27 +71,46 @@ def send_push_notification(applet_id, event_id, activity_id=None, send_time=None
 
         profiles = list(Profile().find(query=query, fields=['deviceId', 'badge']))
 
-        message_title = event['data']['title']
-        message_body = event['data']['description']
+        notification = messaging.Notification(
+            title=event['data']['title'],
+            body=event['data']['description']
+        )
 
-        for profile in profiles:
-            if len(profile['deviceId']):
-                profile['badge'] = profile['badge'] + 1
-                result = push_service.notify_single_device(
-                    registration_id=profile['deviceId'],
-                    badge=profile.get('badge', 0),
-                    message_title=message_title,
-                    message_body=message_body,
-                    data_message={
+        chunk_requests = [profiles[i:i + AMOUNT_MESSAGES_PER_REQUEST] for i in
+                          range(0, len(profiles), AMOUNT_MESSAGES_PER_REQUEST)]
+
+        # Limitation by firebase no more then 230 requests per minute
+        if len(chunk_requests) > REQUESTS_LIMITATION_PER_EVENT:
+            chunk_requests = chunk_requests[0:REQUESTS_LIMITATION_PER_EVENT]
+
+        total_message_chunks = []
+
+        for profiles in chunk_requests:
+            messages = []
+
+            for profile in profiles:
+                messages.append(messaging.Message(
+                    token=profile['deviceId'],
+                    notification=notification,
+                    data={
                         "event_id": str(event_id),
                         "applet_id": str(applet_id),
                         "activity_id": str(activity_id)
-                    }
-                )
-                print(
-                    f'Status - {"failed " + str(result["failure"]) if result["failure"] else "success " + str(result["success"])}')
-                if 'success' in result:
-                    Profile().increment(query={"_id": profile['_id']}, field='badge', amount=1)
+                    },
+                    apns=messaging.APNSConfig(
+                        payload=messaging.APNSPayload(
+                            aps=messaging.Aps(badge=int(profile.get('badge', 0) + 1))
+                        )
+                    )
+                ))
+            total_message_chunks.append(messages)
+
+        for messages in total_message_chunks:
+            messaging.send_all(messages)
+
+        Profile().increment(query={"_id": {
+            "$in": [profile['_id'] for profile in profiles]
+        }}, field='badge', amount=1)
 
         # if random time we will reschedule it in time between 23:45 and 23:59
         if event['data']['notifications'][0]['random'] and now.hour == 23 and 59 >= now.minute >= 45:
