ec4a13b57aa3c9a72da9b52fb6b24f12e1fff2ca
Benjamin Thonet <devtech728@gmail.com>
2020-08-21 19:18:47 +0300

    - scheduler and worker scripts were separate from the main project setup - rq module was updated to newest version to fix the issue with worker timeout

--- girderformindlogger/external/rq_worker.py
+++ girderformindlogger/external/rq_worker.py
@@ -1,7 +1,14 @@
-from rq import Worker
+from rq import SimpleWorker
 
+from girderformindlogger.utility import reconnect
 from girderformindlogger.models import getRedisConnection
 redis = getRedisConnection()
 
-Worker(['default'], connection=redis).work(with_scheduler=True)
 
+@reconnect(name='Worker')
+def start():
+    SimpleWorker(['default'], connection=redis).work(with_scheduler=True)
+
+
+if __name__ == '__main__':
+    start()
--- girderformindlogger/external/scheduler.py
+++ girderformindlogger/external/scheduler.py
@@ -1,6 +1,14 @@
 from rq_scheduler import Scheduler
 
+from girderformindlogger.utility import reconnect
 from girderformindlogger.models import getRedisConnection
 redis = getRedisConnection()
 
-Scheduler(connection=redis, interval=5).run()
+
+@reconnect(name='Scheduler')
+def start():
+    Scheduler(connection=redis, interval=5).run()
+
+
+if __name__ == '__main__':
+    start()
