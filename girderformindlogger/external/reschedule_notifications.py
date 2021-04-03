52449a0cf357466cf89821823af5318d29002ceb
jdev <rjtech0018@gmail.com>
2021-04-03 01:03:36 -0400

    add script for rescheduling all notifications

---
+++ girderformindlogger/external/reschedule_notifications.py
@@ -0,0 +1,11 @@
+import datetime
+
+from bson import ObjectId
+from girderformindlogger.models.events import Events as EventsModel
+from girderformindlogger.models.account_profile import AccountProfile
+
+model = EventsModel()
+events = model.find({})
+
+for event in events:
+    model.setSchedule(event)
