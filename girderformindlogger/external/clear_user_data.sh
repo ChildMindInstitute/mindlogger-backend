252485ef98ef0a97786a45785fa06a6343140bae
jdev <rjtech0018@gmail.com>
2021-07-29 02:05:45 -0400

    update custom scripts

--- girderformindlogger/external/clear_user_data.sh
+++ girderformindlogger/external/clear_user_data.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 source /var/app/venv/staging-LQM1lest/bin/activate
-source ~/env
 cd /var/app/current
 python girderformindlogger/external/clear_user_data.py
--- girderformindlogger/external/rq_scheduler.sh
+++ girderformindlogger/external/rq_scheduler.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 source /var/app/venv/staging-LQM1lest/bin/activate
-source ~/env
 cd /var/app/current
 python girderformindlogger/external/scheduler.py
