7d88bb7b3185f9df70cd87c6525f61f1126d71ff
jdev <rjtech0018@gmail.com>
2021-08-22 13:28:32 -0400

    fix notification

--- girderformindlogger/external/reschedule_notifications.sh
+++ girderformindlogger/external/reschedule_notifications.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 source /var/app/venv/staging-LQM1lest/bin/activate
+source /opt/elasticbeanstalk/deployment/custom_env_var
 cd /var/app/current
 python girderformindlogger/external/reschedule_notifications.py
