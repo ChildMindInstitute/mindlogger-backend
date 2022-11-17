72b3812f02e086de71101f4d81f540e0696d0d5b
Tom Isaak <isoqov12@gmail.com>
2022-11-17 10:11:49 +0300

    add current state of events

--- girderformindlogger/models/applet.py
+++ girderformindlogger/models/applet.py
@@ -230,6 +230,7 @@ class Applet(FolderModel):
         if not getAllEvents:
             schedule = EventsModel().getScheduleForUser(applet['_id'], user['_id'], eventFilter)
             events = schedule.get('events', {})
+            schedule['actual_events'] = dict(events)
 
             for localEvent in localEvents:
                 eventId = localEvent.get('id', None)
