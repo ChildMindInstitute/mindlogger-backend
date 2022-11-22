d55f3ed37c257c918f2f17a81d59320d22d58e4a
Tom Isaak <isoqov12@gmail.com>
2022-11-22 13:45:35 +0300

    change getting events

--- girderformindlogger/models/applet.py
+++ girderformindlogger/models/applet.py
@@ -230,7 +230,7 @@ class Applet(FolderModel):
         if not getAllEvents:
             schedule = EventsModel().getScheduleForUser(applet['_id'], user['_id'], eventFilter)
             events = schedule.get('events', {})
-            actual_events = schedule.get('actual_events', [])
+            actual_events = schedule.pop('actual_events', [])
 
             for localEvent in localEvents:
                 eventId = localEvent.get('id', None)
