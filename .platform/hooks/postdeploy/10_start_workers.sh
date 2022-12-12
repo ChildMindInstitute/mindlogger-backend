e2dd0bd63a898f4a6bc74100f0279e7fabe53821
Roman Sevko <rsiauko@scnsoft.com>
2022-12-12 16:23:26 +0300

    switched from 'restart' to 'delete & start' for pm2 workers

--- .platform/hooks/postdeploy/10_start_workers.sh
+++ .platform/hooks/postdeploy/10_start_workers.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-## (re)start workers
-pm2 restart /var/app/current/girderformindlogger/external/rq_worker.sh --name=worker_1
-pm2 restart /var/app/current/girderformindlogger/external/rq_worker.sh --name=worker_2
+## delete workers
+pm2 delete all || echo 'No workers found'
+
+## start workers
+pm2 start /var/app/current/girderformindlogger/external/rq_worker.sh --name=worker_1
+pm2 start /var/app/current/girderformindlogger/external/rq_worker.sh --name=worker_2
 
 ## start scheduler
-pm2 restart /var/app/current/girderformindlogger/external/rq_scheduler.sh
+pm2 start /var/app/current/girderformindlogger/external/rq_scheduler.sh
